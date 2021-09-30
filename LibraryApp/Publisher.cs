bc07c180b4c5afbf68a2616a9dc5f6493d370e90
AlexMayk <alexandramaykowa518@gmail.com>
2021-09-30 10:27:30 +0300

    8. New table Publishers was added, was added connection one-many between tables Publishers and Books

--- LibraryApp/Publisher.cs
+++ LibraryApp/Publisher.cs
@@ -12,14 +12,18 @@ namespace LibraryApp
     using System;
     using System.Collections.Generic;
     
-    public partial class Book
+    public partial class Publisher
     {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Publisher()
+        {
+            this.Books = new HashSet<Book>();
+        }
+    
         public int Id { get; set; }
         public string Name { get; set; }
-        public bool IsActive { get; set; }
-        public int Publisher_id { get; set; }
     
-        public virtual BookDetail BookDetail { get; set; }
-        public virtual Publisher Publisher { get; set; }
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Book> Books { get; set; }
     }
 }
