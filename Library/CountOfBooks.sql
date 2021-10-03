05f233674baa8d7b6804367b077430903a213956
AlexMayk <alexandramaykowa518@gmail.com>
2021-10-03 14:07:08 +0300

    12. Was added new function CountOfBooks

---
+++ Library/CountOfBooks.sql
@@ -0,0 +1,13 @@
+﻿CREATE FUNCTION [dbo].[CountOfBooks]
+(
+	@authorId int
+)
+RETURNS int 
+AS  
+BEGIN
+RETURN
+(
+	 SELECT COUNT(Books.Id) FROM [Books] right join AuthorBook on AuthorBook.BookId=Books.Id WHERE AuthorBook.AuthorId=@authorId
+)
+END
+
